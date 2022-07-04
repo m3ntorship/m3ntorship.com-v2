b2be1081b8e48f4985c6629114b9e2e7c4332155
Ali-Khamis <al.tramsia@gmail.com@gmail.com>
2022-07-04 22:57:41 +0200

    config: added tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,64 +1,104 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const { fontFamily } = require('tailwindcss/defaultTheme');
+const {
+  oneLayerWithPixelAdding,
+  themeFontFamily,
+  oneLayerWithNoPixel,
+  themeShadowBox,
+  themeLetterSpacing,
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+} = require('./utils/tailwind/tailwindHelpers.js');
 
-function withOpacityValue(variable) {
-  return ({ opacityValue }) => {
-    if (opacityValue === undefined) {
-      return `rgb(var(${variable}))`;
-    }
-    return `rgb(var(${variable}) / ${opacityValue})`;
-  };
-}
-
-/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {
-      fontFamily: {
-        primary: ['Inter', ...fontFamily.sans],
-      },
-      colors: {
-        primary: {
-          // Customize it on globals.css :root
-          50: withOpacityValue('--tw-color-primary-50'),
-          100: withOpacityValue('--tw-color-primary-100'),
-          200: withOpacityValue('--tw-color-primary-200'),
-          300: withOpacityValue('--tw-color-primary-300'),
-          400: withOpacityValue('--tw-color-primary-400'),
-          500: withOpacityValue('--tw-color-primary-500'),
-          600: withOpacityValue('--tw-color-primary-600'),
-          700: withOpacityValue('--tw-color-primary-700'),
-          800: withOpacityValue('--tw-color-primary-800'),
-          900: withOpacityValue('--tw-color-primary-900'),
-        },
-        dark: '#222222',
-      },
-      keyframes: {
-        flicker: {
-          '0%, 19.999%, 22%, 62.999%, 64%, 64.999%, 70%, 100%': {
-            opacity: 0.99,
-            filter:
-              'drop-shadow(0 0 1px rgba(252, 211, 77)) drop-shadow(0 0 15px rgba(245, 158, 11)) drop-shadow(0 0 1px rgba(252, 211, 77))',
-          },
-          '20%, 21.999%, 63%, 63.999%, 65%, 69.999%': {
-            opacity: 0.4,
-            filter: 'none',
-          },
-        },
-        shimmer: {
-          '0%': {
-            backgroundPosition: '-700px 0',
-          },
-          '100%': {
-            backgroundPosition: '700px 0',
-          },
-        },
-      },
-      animation: {
-        flicker: 'flicker 3s linear infinite',
-        shimmer: 'shimmer 1.3s linear infinite',
-      },
+    colors: {
+      // Primary Colors
+      primary: 'rgba(var(--color-primary), <alpha-value>)',
+      'primary-contrast': 'rgba(var(--color-primary-contrast), 1)',
+      'primary-hover': 'rgba(var(--color-primary-hover), 1)',
+      'primary-ghost-hover': 'rgba(var(--color-primary), 0.13)',
+      'primary-border': 'rgba(var(--color-primary), 0.7)',
+      // Secondary Colors
+      secondary: 'rgba(var(--color-secondary), <alpha-value>)',
+      'secondary-contrast': 'rgba(var(--color-secondary-contrast), 1)',
+      'secondary-hover': 'rgba(var(--color-secondary-hover), 1)',
+      'secondary-ghost-hover': 'rgba(var(--color-secondary), 0.13)',
+      'secondary-border': 'rgba(var(--color-secondary), 0.7)',
+      // Text Colors
+      'primary-text': 'rgba(var(--color-text), 1)',
+      'secondary-text': 'rgba(var(--color-text), 0.6)',
+      'disabled-text': 'rgba(var(--color-text), 0.3)',
+      // Background Colors
+      background: 'rgba(var(--color-background), <alpha-value>)',
+      'background-surface': 'rgba(var(--color-background-surface), 1)',
+      // Neutral Colors
+      neutral: 'rgba(var(--color-neutral), <alpha-value>)',
+      'neutral-light': 'rgba(var(--color-neutral), 0.6)',
+      'neutral-lighter': 'rgba(var(--color-neutral), 0.3)',
+      'neutral-fade': 'rgba(var(--color-neutral), 0.1)',
+      // Error Colors
+      error: 'rgba(var(--color-error), <alpha-value>)',
+      'error-contrast': 'rgba(var(--color-error-contrast), 1)',
+      'error-hover': 'rgba(var(--color-error-hover), 1)',
+      'error-ghost-hover': 'rgba(var(--color-error), 0.13)',
+      'error-border': 'rgba(var(--color-error), 0.7)',
+      'error-content': 'rgba(var(--color-error-content), 1)',
+      'error-background': 'rgba(var(--color-error), 0.1)',
+      // Info Colors
+      info: 'rgba(var(--color-info), <alpha-value>)',
+      'info-contrast': 'rgba(var(--color-info-contrast), 1)',
+      'info-hover': 'rgba(var(--color-info-hover), 1)',
+      'info-ghost-hover': 'rgba(var(--color-info), 0.13)',
+      'info-border': 'rgba(var(--color-info), 0.7)',
+      'info-content': 'rgba(var(--color-info-content), 1)',
+      'info-background': 'rgba(var(--color-info), 0.1)',
+      // Warning Colors
+      warning: 'rgba(var(--color-warning), <alpha-value>)',
+      'warning-contrast': 'rgba(var(--color-warning-contrast), 1)',
+      'warning-hover': 'rgba(var(--color-warning-hover), 1)',
+      'warning-ghost-hover': 'rgba(var(--color-warning), 0.13)',
+      'warning-border': 'rgba(var(--color-warning), 0.7)',
+      'warning-content': 'rgba(var(--color-warning-content), 1)',
+      'warning-background': 'rgba(var(--color-warning), 0.1)',
+      // Success Colors
+      success: 'rgba(var(--color-success), <alpha-value>)',
+      'success-contrast': 'rgba(var(--color-success-contrast), 1)',
+      'success-hover': 'rgba(var(--color-success-hover), 1)',
+      'success-ghost-hover': 'rgba(var(--color-success), 0.13)',
+      'success-border': 'rgba(var(--color-success), 0.7)',
+      'success-content': 'rgba(var(--color-success-content), 1)',
+      'success-background': 'rgba(var(--color-success), 0.1)',
+      // Action Colors
+      action: 'rgba(var(--color-action), <alpha-value>)',
+      'action-disabled': 'rgba(var(--color-action), 0.5)',
+      'action-background-selected': 'rgba(var(--color-action), 0.3)',
+      'action-background-disabled': 'rgba(var(--color-action), 0.15)',
+      'action-background-hover': 'rgba(var(--color-action), 0.1)',
+      // Misc Colors
+      'misc-snackbar-bg': 'rgba(var(--color-misc), 0.8)',
+      'misc-overlay': 'rgba(var(--color-misc), 0.6)',
+      'misc-standard-border': 'rgba(var(--color-misc), 0.3)',
+      'misc-divider': 'rgba(var(--color-misc), 0.1)',
+      'misc-rating': 'rgba(var(--color-misc-rating), <alpha-value>)',
+      white: 'rgb(255, 255, 255)',
+      black: 'rgb(0, 0, 0)',
+      transparent: 'transparent',
+    },
+    fontSize: oneLayerWithPixelAdding('fontSize'),
+    lineHeight: oneLayerWithPixelAdding('lineHeight'),
+    fontFamily: themeFontFamily(),
+    fontWeight: oneLayerWithNoPixel('fontWeight'),
+    spacing: oneLayerWithPixelAdding('spacing'),
+    borderRadius: oneLayerWithPixelAdding('brd-radius'),
+    opacity: oneLayerWithNoPixel('opacity'),
+    borderWidth: oneLayerWithPixelAdding('brd-width'),
+    ringWidth: oneLayerWithPixelAdding('ring-width'),
+    boxShadow: themeShadowBox(),
+    letterSpacing: themeLetterSpacing(),
+    screens: {
+      small: '0',
+      medium: '600px',
+      large: '900px',
+      xlarge: '1301px',
     },
   },
   plugins: [require('@tailwindcss/forms')],
