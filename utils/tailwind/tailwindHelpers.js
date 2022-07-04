89aff7376c30218d079888d4dbb55ca99bdc3189
Ali-Khamis <al.tramsia@gmail.com@gmail.com>
2022-07-04 23:22:45 +0200

    fix: removed mutation of the main token in the functions

--- utils/tailwind/tailwindHelpers.js
+++ utils/tailwind/tailwindHelpers.js
@@ -1,30 +1,29 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const tailwindThemeTokens = require('./figmaTokens.json');
 
-
 const oneLayerWithPixelAdding = itemName => {
-  const fontSizes = tailwindThemeTokens[itemName];
+  const fontSizes = {...tailwindThemeTokens[itemName]};
   Object.keys(fontSizes).map(property => {
-    fontSizes[property] = `${fontSizes[property].value}px`;
+    if(fontSizes[property]?.value)fontSizes[property] = `${fontSizes[property]?.value}px`;
   });
   return fontSizes;
 };
 const themeFontFamily = () => {
-  const fontFamily = tailwindThemeTokens['fontFamily'];
+  const fontFamily = {...tailwindThemeTokens['fontFamily']};
   Object.keys(fontFamily).map(property => {
     fontFamily[property] = [fontFamily[property]['value']];
   });
   return fontFamily;
 };
 const oneLayerWithNoPixel = itemName => {
-  const fontWeight = tailwindThemeTokens[itemName];
+  const fontWeight = {...tailwindThemeTokens[itemName]};
   Object.keys(fontWeight).map(property => {
-    fontWeight[property] = fontWeight[property].value;
+    fontWeight[property] = fontWeight[property]?.value;
   });
   return fontWeight;
 };
 const themeShadowBox = () => {
-  const boxShadow = tailwindThemeTokens['boxShadow'];
+  const boxShadow = {...tailwindThemeTokens['boxShadow']};
   Object.keys(boxShadow).map(property => {
     boxShadow[property] = Object.keys(boxShadow[property]['value'])
       .map(item => {
@@ -38,9 +37,9 @@ const themeShadowBox = () => {
   return boxShadow;
 };
 const themeLetterSpacing = () => {
-  const letterSpacing = tailwindThemeTokens['letterSpacing'];
+  const letterSpacing = {...tailwindThemeTokens['letterSpacing']};
   Object.keys(letterSpacing).map(property => {
-    const valueWithNoPercentage = letterSpacing[property].value.replace(
+    const valueWithNoPercentage = letterSpacing[property]?.value?.replace(
       '%',
       ''
     );
