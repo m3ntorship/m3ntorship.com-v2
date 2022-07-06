a898c96eec504caa1e476778ae7986206ae90ac6
Abdulrahman Elhaw <abd.elsayed@hotmail.com>
2022-07-06 12:13:49 +0000

    feat: added JoiningProcessFlow compoent (styles,html, types)

---
+++ src/modules/HomePage/components/JoiningProcessFlow/index.tsx
@@ -0,0 +1,58 @@
+import { Typography } from 'm3ntorship-ui';
+import type { FC, ReactElement } from 'react';
+
+import styles from './JoiningProcessFlow.module.css';
+
+import ProcessCard from '@/modules/HomePage/components/JoiningProcessFlow/ProcessCard';
+
+const processSteps = [
+  {
+    title: 'Apply to a program',
+    description:
+      'Prepare some of your work examples and apply to your preferred program',
+    order: 1,
+  },
+  {
+    title: 'Work on a real project',
+    description:
+      'Once you get accepted, you will work on real projects along with other mentees',
+    order: 2,
+  },
+  {
+    title: 'Graduate & give back',
+    description:
+      'Graduate and give back to the community by mentoring new batches',
+    order: 3,
+  },
+];
+const JoiningProcessFlow: FC = (): ReactElement => {
+  return (
+    <div className='bg-background'>
+      <div className='container mx-auto'>
+        <div className='flex flex-col gap-xs'>
+          <Typography className={styles.card_title} align='center' variant='h1'>
+            How the process works
+          </Typography>
+          <Typography align='center' variant='subtitle'>
+            Consectetur adipiscing elit duis tristique sollicitudin nibh sit
+            amet commodo.
+          </Typography>
+        </div>
+        <div className='grid gap-med  py-xxxxl small:grid-cols-1 small:gap-x-med large:grid-cols-3 large:gap-xl'>
+          {processSteps.map(({ description, title, order }, index) => {
+            return (
+              <ProcessCard
+                key={index}
+                title={title}
+                description={description}
+                order={order}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default JoiningProcessFlow;
