2a017825a1c73282ef0e1c3930344371bc0baa3f
atwfk <ahmed.twfiek8@gmail.com>
2022-07-16 13:30:01 +0200

    style: apply the new screens width in footer section

--- src/modules/HomePage/index.tsx
+++ src/modules/HomePage/index.tsx
@@ -3,6 +3,7 @@ import type { ReactElement } from 'react';
 import HeroSection from '@/modules/HomePage/components/HeroSection';
 import { JoiningProcessFlow } from '@/modules/HomePage/components/organisms';
 import ValueSection from '@/modules/HomePage/components/ValueSection';
+import { Footer } from '@/shared/components/molecules';
 
 export default function HomePage(): ReactElement {
   return (
@@ -10,6 +11,7 @@ export default function HomePage(): ReactElement {
       <HeroSection />
       <ValueSection />
       <JoiningProcessFlow />
+      <Footer />
     </>
   );
 }
