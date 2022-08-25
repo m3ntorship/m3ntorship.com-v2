b66635eb24deabb4011d67c1426f7e5e7b917817
AhmedAdel999 <ahmedadel555441@gmail.com>
2022-08-25 12:33:13 +0200

    feat: added menteeApllicationStepTwo

--- src/modules/ApplicationsPage/components/molecules/index.tsx
+++ src/modules/ApplicationsPage/components/molecules/index.tsx
@@ -1,3 +1,4 @@
 export { default as ApplicationsHeader } from './ApplicationsHeader';
 export { default as ApplicationsTab } from './ApplicationsTab';
 export { default as MenteeApplicationStepOne } from './MenteeApplicationStepOne';
+export { default as MenteeApplicationStepTwo } from './MenteeApplicationStepTwo';
