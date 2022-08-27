978d8105fbad75d7340cd74d8007f70fe97d30da
atwfk <ahmed.twfiek8@gmail.com>
2022-08-27 15:51:02 +0200

    feat: add form control and validation on mentor application

--- src/shared/constants/index.ts
+++ src/shared/constants/index.ts
@@ -2,3 +2,6 @@ export const isDevelopment = process.env.NEXT_PUBLIC_APP_ENV === 'development';
 
 export const websitePattern =
   /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/i;
+
+export const linkedinUrlPattern =
+  /https?:\/\/(www\.)?linkedin.com\/(in|company)\/[a-zA-Z0-9-]{5,30}/i;
