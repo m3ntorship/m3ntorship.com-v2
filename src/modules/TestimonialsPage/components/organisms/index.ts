28c28f6c1450703c5f33919c52968f3a3f8608a6
atwfk <ahmed.twfiek8@gmail.com>
2022-10-05 08:35:49 +0200

    fix: move TestimonialCard to molecules folder

--- src/modules/TestimonialsPage/components/organisms/index.ts
+++ src/modules/TestimonialsPage/components/organisms/index.ts
@@ -1,3 +1,2 @@
-export { default as TestimonialCard } from './TestimonialCard';
 export { default as TestimonialsHero } from './TestimonialsHero';
 export { EnrollSection } from '@/shared/components/molecules';
