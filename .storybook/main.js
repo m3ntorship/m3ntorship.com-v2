5c16b424cb5f0821a9190ac76e5a7e092eb15fe3
abdulrahman <abd.elsayed@hotmail>
2022-10-10 12:19:59 +0200

    fix: fix format error

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,14 +1,11 @@
 const path = require('path');
 module.exports = {
-  stories: [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
+  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "storybook-css-modules-preset",
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-interactions',
+    'storybook-css-modules-preset',
     {
       name: '@storybook/addon-postcss',
       options: {
